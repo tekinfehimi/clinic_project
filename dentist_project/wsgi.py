e94dd3db13fc34c321af3dd157a2aa3e9faa81d3
takinfahimi <gfehimi@gmail.com>
2025-07-11 16:12:20 +0400

    changed wsgi

--- dentist_project/wsgi.py
+++ dentist_project/wsgi.py
@@ -1,3 +1,20 @@
+# """
+# WSGI config for dentist_project project.
+
+# It exposes the WSGI callable as a module-level variable named ``application``.
+
+# For more information on this file, see
+# https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+# """
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dentist_project.settings')
+
+# application = get_wsgi_application()
+
 """
 WSGI config for dentist_project project.
 
@@ -8,9 +25,15 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dentist_project.settings')
 
 application = get_wsgi_application()
+
+# 💡 MÜVƏQQƏTİ MİGRATE ƏMRİ:
+import django
+from django.core.management import call_command
+
+django.setup()
+call_command("migrate")
