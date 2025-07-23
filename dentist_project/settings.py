3c093f8a2fd0465c78ffb74101fc681a5c9c4e73
takinfahimi <gfehimi@gmail.com>
2025-07-23 07:58:17 +0400

    changed settings

--- dentist_project/settings.py
+++ dentist_project/settings.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
